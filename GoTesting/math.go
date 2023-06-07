855a27bb9fe2c5fa35ee365442283a3a18acf827
Deepsresthi <deepsresthi@gmail.com>
2023-06-07 20:57:52 +0530

    first commit

---
+++ GoTesting/math.go
@@ -0,0 +1,9 @@
+package math
+
+func Add(x, y int) (res int){
+	return x+y
+}
+
+func Substract(x, y int) (res int) {
+	return x-y
+}
---
+++ GoTesting/math_test.go
@@ -0,0 +1,49 @@
+package math
+
+import (
+	"testing"
+	"fmt"	
+)
+
+// func TestAdd(t *testing.T) {
+
+// 	got := Add(4, 6)
+// 	want := 10
+
+// 	if got != want {
+// 		t.Errorf("got %q, wanted %q", got, want)
+// 	}
+// }
+
+type addTest struct{
+	arg1, arg2, expected int
+}
+
+//arg1 and arg2 are argumenst, and the expected stands for the 'result we expect' 
+
+var addTests = []addTest{
+	{2, 3, 5},
+	{2, 8, 10},
+	{5, 7, 12},
+	{3, 13, 16},
+}
+
+func TestAdd(t *testing.T){
+	
+	for _, test := range addTests{
+		if output := Add(test.arg1, test.arg2); output != test.expected {
+			t.Errorf("Output %q not equal to expecetd %q", output, test.expected)
+		}
+	}
+}
+
+func BenchmarkAdd(b *testing.B) {
+	for i:=0; i<b.N; i++{
+		Add(4, 6)
+	}
+}
+
+func ExampleAdd() {
+	fmt.Println(Add(4,6))
+	//output 10
+}
